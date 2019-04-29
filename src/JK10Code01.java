8402d7d65957a77d33e2138afd1869780b9387c0
lvxingxing <18734825542@163.com>
2019-04-29 17:17:30 +0800

    commit leecode 70

--- src/JK10Code01.java
+++ src/JK10Code01.java
@@ -16,6 +16,25 @@ public class JK10Code01 {
         map.put(n,result);
         return result;
     }
+
+    //动态规划
+    public int climbStairs(int n) {
+        // base cases
+        if(n <= 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+
+        int one_step_before = 2;
+        int two_steps_before = 1;
+        int all_ways = 0;
+
+        for(int i=2; i<n; i++){
+            all_ways = one_step_before + two_steps_before;
+            two_steps_before = one_step_before;
+            one_step_before = all_ways;
+        }
+        return all_ways;
+    }
 }
 
 
