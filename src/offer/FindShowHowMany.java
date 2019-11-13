8ed613e685badca6d33389b7782778eee5641c2f
lvxingxing <lvxingxing@xiaomi.com>
2019-11-13 16:23:14 +0800

    coding

---
+++ src/offer/FindShowHowMany.java
@@ -0,0 +1,21 @@
+package offer;
+
+public class FindShowHowMany {
+    public int GetNumberOfK(int [] array , int k) {
+        return binarySearch(array,k + 0.5) - binarySearch(array,k - 0.5);
+    }
+
+    public int binarySearch(int[] arr,double target){
+        int left = 0;
+        int right = arr.length - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(arr[mid] > target){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
