a788075a9ac65179c3749a3b9b67967bff2e4bff
xingxing <18734825542@163.com>
2021-11-04 17:19:55 +0800

    day15

---
+++ src/dp_study_project/LeetCode_cn_62.java
@@ -0,0 +1,24 @@
+package dp_study_project;
+
+public class LeetCode_cn_62 {
+    public int uniquePaths(int m, int n) {
+        if(m == 1 && n ==1){
+            return 1;
+        }
+        int[][] dp = new int[m][n];
+
+        for(int i = 1;i < m;i ++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1;i < n;i ++){
+            dp[0][i] = 1;
+        }
+
+        for(int i = 1;i < m;i ++){
+            for(int j = 1;j < n;j ++){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
--- src/dp_study_project/LeetCode_cn_740.java
+++ src/dp_study_project/LeetCode_cn_740.java
@@ -10,10 +10,37 @@ import java.util.ArrayList;
  * 开始你拥有 0 个点数。返回你能通过这些操作获得的最大点数。
  *
  *
- * https://leetcode-cn.com/problems/delete-and-earn/solution/ru-guo-ni-li-jie-liao-da-jia-jie-she-zhe-ti-ni-ken/
  */
 
 public class LeetCode_cn_740 {
+
+
+    /**
+     * https://leetcode-cn.com/problems/delete-and-earn/solution/gong-shui-san-xie-zhuan-huan-wei-xu-lie-6c9t0/
+     */
+    int[] cnts = new int[10009];
+    public int deleteAndEarn1(int[] nums) {
+        int n = nums.length;
+        int max = 0;
+        for (int x : nums) {
+            cnts[x]++;
+            max = Math.max(max, x);
+        }
+        // f[i][0] 代表「不选」数值 i；f[i][1] 代表「选择」数值 i
+        int[][] f = new int[max + 1][2];
+        for (int i = 1; i <= max; i++) {
+            f[i][1] = f[i - 1][0] + i * cnts[i];
+            f[i][0] = Math.max(f[i - 1][1], f[i - 1][0]);
+        }
+        return Math.max(f[max][0], f[max][1]);
+    }
+
+
+    /**
+     *  * https://leetcode-cn.com/problems/delete-and-earn/solution/ru-guo-ni-li-jie-liao-da-jia-jie-she-zhe-ti-ni-ken/
+     * @param nums
+     * @return
+     */
     public int deleteAndEarn_dp(int[] nums) {
         if (nums == null || nums.length == 0){
             return 0;
