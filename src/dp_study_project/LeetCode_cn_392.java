2097992097ceb62b914940c65e11ca538e626bc1
xingxing <18734825542@163.com>
2021-11-08 17:44:42 +0800

    day19

---
+++ src/dp_study_project/LeetCode_cn_392.java
@@ -0,0 +1,32 @@
+package dp_study_project;
+
+public class LeetCode_cn_392 {
+    public boolean isSubsequence(String s, String t) {
+        int n = s.length(), m = t.length();
+
+        int[][] f = new int[m + 1][26];
+        for (int i = 0; i < 26; i++) {
+            f[m][i] = m;
+        }
+
+        //预处理
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 0; j < 26; j++) {
+                if (t.charAt(i) == j + 'a')
+                    f[i][j] = i;
+                else
+                    f[i][j] = f[i + 1][j];
+            }
+        }
+        //匹配
+        int add = 0;
+        for (int i = 0; i < n; i++) {
+            if (f[add][s.charAt(i) - 'a'] == m) {
+                return false;
+            }
+            add = f[add][s.charAt(i) - 'a'] + 1;
+        }
+        return true;
+    }
+
+}
