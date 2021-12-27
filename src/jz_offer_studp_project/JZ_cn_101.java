e44d04be4f2a921827aae49ae3fbfc4f31016abc
LVXINGXING <18734825542@163.com>
2021-12-27 19:52:41 +0800

    JZ101-1

--- src/jz_offer_studp_project/JZ_cn_101.java
+++ src/jz_offer_studp_project/JZ_cn_101.java
@@ -1,6 +1,34 @@
 package jz_offer_studp_project;
 
 public class JZ_cn_101 {
+
+    public boolean canPartition1(int[] nums) {
+        if(nums.length  < 2){
+            return false;
+        }
+        int sum = 0;
+        for(int i = 0;i < nums.length;i ++){
+            sum += nums[i];
+        }
+        if(sum % 2 != 0){
+            return false;
+        }
+        sum = sum / 2;
+        boolean[][] dp = new boolean[nums.length + 1][sum + 1];
+        dp[0][0] = true;
+        for(int i = 1;i <= nums.length;i ++){
+            for(int j = 0;j <= sum; j ++){
+                dp[i][j] = dp[i - 1][j];
+                if(!dp[i][j] && j - nums[i - 1] >= 0){
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[nums.length][sum];
+
+    }
+
+    //空间压缩
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         if(n <= 1){
