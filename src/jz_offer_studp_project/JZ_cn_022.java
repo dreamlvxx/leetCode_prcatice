3977892c465981b26fbd77be56ff05fd4af5ee2a
xingxing <18734825542@163.com>
2022-03-22 08:02:10 +0800

    offer022

---
+++ src/jz_offer_studp_project/JZ_cn_022.java
@@ -0,0 +1,34 @@
+package jz_offer_studp_project;
+
+public class JZ_cn_022 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                fast = head;
+                while (fast != slow) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
